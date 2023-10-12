83a18b9b8fb49edbead46d740772d7ce558da36e
escott2 <etriplett14@gmail.com>
2023-10-11 20:49:12 -0500

    portals: add initial files

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <>
+      <h1>Portals</h1>
+      <div className="card">
+      <p>Create a portal</p>
+      <p>View portals</p>
+      </div>
+    </>
+  )
+}
+
+export default Home;
